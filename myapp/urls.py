dc9aae6bb7462dcae1961d87ae5b853b471aad11
root <root@localhost.localdomain>
2018-08-31 20:50:44 +0900

    2

--- myapp/urls.py
+++ myapp/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('polyline/', include('polyline.urls')),
 ]
---
+++ polyline/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PolylineConfig(AppConfig):
+    name = 'polyline'
